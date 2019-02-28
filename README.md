30862b83e1718b9f2c7781ce2545de7ae7aa61a4
Alex Savatieiev <a.savex@gmail.com>
2019-02-28 17:27:49 -0600

    setup.py

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-# salt-ci-packages
-Gather packages installed on listed minions 
-and create a fancy report with version comparison
+# salt-cfg-checker
+This checker module is used to verify and validate cloud
+after the deployment using number of routines to create reports
 
-# Notes
+# Local run
+It is pretty simple: 
+- deploy on cfg node
+- check your local.env file for parameters
+- ...run: `cfg_check -h`
+
+# External cloud
 Be sure to 
- - create your *.env file 
+ - create your *.env file
    or supply environment vars to module other way
- - execute `salt-key` on master 
+ - in the `env` file set host and user for SSH. 
+   For options, use your `~/.ssh/config`
+ - if node listing fails, execute `salt-key` on master 
    and create an `etc/nodes.list` file with minions list
+
+To be updated.
 
 Cheers!
 
---
+++ setup.py
@@ -0,0 +1,52 @@
+import glob
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+DATA = [
+    ('etc', [f for f in glob.glob(os.path.join('etc', '*'))]),
+    ('templates', [f for f in glob.glob(os.path.join('templates', '*'))]),
+#    ('res', [f for f in glob.glob(os.path.join('res', '*'))])
+]
+
+dependencies = [
+    'six',
+    'pyyaml',
+    'jinja2',
+    'requests',
+    'ipaddress'
+]
+
+entry_points = {
+    "console_scripts":
+        "cfg_checker = cfg_checker:cfg_check"
+}
+
+
+setup(
+    name="Mirantis Cloud Configuration Checker",
+    version="0.1",
+    author="Alex Savatieiev",
+    author_email="osavatieiev@mirantis.com",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7"
+    ],
+    keywords="QA, openstack, salt, config, reclass",
+    entry_points=entry_points,
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        '': ['*.conf', '*.env', '*.list', '*.html']
+    },
+    zip_safe=False,
+    install_requires=dependencies,
+    data_files=DATA,
+    license="Apache Licence, version 2",
+    description="MCP Checker tool. For best results use on MCP deployments",
+    long_description=README
+)
